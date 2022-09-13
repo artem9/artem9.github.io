32b6c0a7f5f7a72bda86a296936edb0e246e3b6f
Artem Suzdal <asuzdal@spscommerce.com>
2022-09-13 22:24:35 +0300

    Project init

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  resolve: { extensions: ['.tsx', '.ts', '.js'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: path.join(__dirname, 'public/'),
+    devMiddleware: {
+      publicPath: '/dist/'
+    },
+    port: 3000,
+    hot: 'only'
+  }
+};
